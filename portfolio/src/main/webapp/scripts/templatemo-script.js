35f0f530c3665a88d223f92d91c642903f189894
Michaela <michaelatang99@gmail.com>
2020-06-18 05:06:34 +0800

    Create basic comment box, add comments to comment box

--- portfolio/src/main/webapp/scripts/templatemo-script.js
+++ portfolio/src/main/webapp/scripts/templatemo-script.js
@@ -46,11 +46,37 @@ $(document).ready(function () {
     document.querySelector('.tm-current-year').textContent = new Date().getFullYear();
 });
 
+var pictures = new Array("images/apollo.jpg", "images/bob.jpeg", "images/boomer.jpg", "images/drift.jpg",
+                        "images/filbert.jpg", "images/kiki.jpg", "images/lolly.jpg", "images/punchy.jpg",
+                        "images/rosie.jpg");
+
+/**
+ * Fetch the comment data in Json format and create the comments in the comments section.
+ */
 async function fetchJson() {
     const response = await fetch('/data');
     const json = await response.json();
-    
-    var comments = json.reduce((prev, comment) => prev + "<p>" + comment + "</p>", "");
 
-    document.querySelector('#comments').innerHTML = comments;
+    var template = document.querySelector('#comment-template');
+    var commentBox = template.content.querySelector('div');
+    var temp;
+
+    json.forEach(comment => {
+        temp = modifyTemplate(commentBox, comment);
+        document.querySelector('#comments').appendChild(temp);
+    });
+}
+
+/**
+ * Replace the icon and text of the template comment box with a random icon and the comment.
+ */
+function modifyTemplate(commentBox, comment) {
+    const tempBox = document.importNode(commentBox, true);
+
+    var randomNum = Math.floor(Math.random() * pictures.length);
+    tempBox.querySelector('#comment-icon').src = pictures[randomNum];
+
+    tempBox.querySelector('#comment-description').textContent = comment;
+
+    return tempBox;
 }
